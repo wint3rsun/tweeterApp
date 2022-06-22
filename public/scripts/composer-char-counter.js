e8a105bb8f2bf188f210568ae4ec376d8b4cfa1a
Abigail Addae <abigail2addae@gmail.com>
2022-06-22 04:50:19 +0000

    create js for tweet text area input event

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+const CHAR_LIMIT = 140;
+
+$(document).ready(function() {
+
+  $("#tweet-text").on("input", function() {
+    let inputTextLength = this.value.length;
+    let output = $(this).siblings().find("output");
+
+    output.val(`${CHAR_LIMIT - inputTextLength}`);
+    
+    (output.val() < 0) ? output.addClass("invalid") : output.removeClass("invalid");
+  });
+
+});
