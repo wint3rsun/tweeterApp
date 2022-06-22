95794b7d5c9f0e68409a1f229eab94298390f58b
Abigail Addae <abigail2addae@gmail.com>
2022-06-22 20:43:01 +0000

    refactor composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,8 @@
 //const CHAR_LIMIT = 140;
 
 $(document).ready(function() {
-  console.log("u working?");
 
   $("#tweet-text").on("input", function() {
-    console.log("still working????");
     let inputTextLength = this.value.length;
     let output = $(this).siblings().find("output");
 
