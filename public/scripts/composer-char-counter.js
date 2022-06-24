75992ffbd3e029b3ce88909732cc7129aa5cacdc
Abigail Addae <abigail2addae@gmail.com>
2022-06-24 23:14:03 +0000

    clean code and add comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
-//const CHAR_LIMIT = 140;
+//const CHAR_LIMIT = 140; was defined in client.js
 
 $(document).ready(function() {
-
+  // calculate value of char remaining in tweet submission form
   $("#tweet-text").on("input", function() {
     let inputTextLength = this.value.length;
     let output = $(this).siblings().find("output");
@@ -10,7 +10,7 @@ $(document).ready(function() {
     
     (output.val() < 0) ? output.addClass("invalid") : output.removeClass("invalid");
   });
-
+  // determine appearance/disapearance of scroll up button on window scroll
   $(window).scroll(function() {
     $('#jquery-ajax-form-submit').hide();
 
